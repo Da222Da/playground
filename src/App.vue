f0830f2246521e68be3b66b54407925fc725bc76
meiyunchao <2511633874@qq.com>
2025-01-08 18:11:57 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="replContainer">
-    <Repl :editor="Monaco" :store="store" :showCompileOutput="false" :showTsConfig="false" />
+    <Repl :editor="Monaco" :store="store" :showCompileOutput="true" :showTsConfig="false" :previewOptions="previewOptions" />
   </div>
 </template>
 
 <script setup>
-import { watchEffect } from "vue";
+import { watchEffect, ref } from "vue";
 import { Repl, useStore, useVueImportMap } from "@vue/repl";
 import Monaco from "@vue/repl/monaco-editor";
+import Vue3Component from "zhangxianjue-vue3-components";
 
-const { importMap: builtinImportMap } = useVueImportMap({
-  // serverRenderer: 'cdn link to server-renderer.esm-browser.js',
+const customStyle = () => {
+  const link = '<link rel="stylesheet" href="https://unpkg.com/zhangxianjue-vue3-components/dist/style.css" />';
+  const style = "<style>body{ margin: 15px 10px;}</style>";
+  return link + style;
+};
+const previewOptions = {
+  headHTML: customStyle(),
+};
+
+// 安装默认依赖
+const builtinImportMap = ref({
+  imports: {
+    vue: "https://cdn.jsdelivr.net/npm/@vue/runtime-dom@3.5.13/dist/runtime-dom.esm-browser.js",
+    "vue/server-renderer": "https://cdn.jsdelivr.net/npm/@vue/server-renderer@3.5.13/dist/server-renderer.esm-browser.js",
+    "zhangxianjue-vue3-components": "https://unpkg.com/zhangxianjue-vue3-components/dist/index.es.js",
+  },
 });
 
 const store = useStore(
   {
-    builtinImportMap,
+    builtinImportMap: builtinImportMap,
     showOutput: true,
     outputMode: "preview",
   },
