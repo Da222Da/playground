de9f461a36c8ce9a97fede05a5da42542a4d1266
meiyunchao <2511633874@qq.com>
2025-01-09 10:20:41 +0800

    'deploy'

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="replContainer">
-    <Repl :editor="Monaco" :store="store" :showCompileOutput="false" :showTsConfig="false" :previewOptions="previewOptions" />
+    <Repl :editor="Monaco" :store="store" :showCompileOutput="false" :showTsConfig="false" :previewOptions="previewOptions" :showImportMap="false" />
   </div>
 </template>
 
 <script setup>
 import { watchEffect, ref } from "vue";
-import { Repl, useStore, useVueImportMap } from "@vue/repl";
+import { Repl, useStore } from "@vue/repl";
 import Monaco from "@vue/repl/monaco-editor";
 
 const customStyle = () => {
-  const link = '<link rel="stylesheet" href="https://unpkg.com/zhangxianjue-vue3-components/dist/style.css" />';
+  const link = '<link rel="stylesheet" href="https://unpkg.com/vitepress-vue3-components/dist/style.css" />';
   const style = "<style>body{ margin: 15px 10px;}</style>";
   return link + style;
 };
@@ -23,7 +23,7 @@ const builtinImportMap = ref({
   imports: {
     vue: "https://cdn.jsdelivr.net/npm/@vue/runtime-dom@3.5.13/dist/runtime-dom.esm-browser.js",
     "vue/server-renderer": "https://cdn.jsdelivr.net/npm/@vue/server-renderer@3.5.13/dist/server-renderer.esm-browser.js",
-    "zhangxianjue-vue3-components": "https://unpkg.com/zhangxianjue-vue3-components/dist/index.es.js",
+    "vitepress-vue3-components": "https://unpkg.com/vitepress-vue3-components/dist/index.es.js",
   },
 });
 
